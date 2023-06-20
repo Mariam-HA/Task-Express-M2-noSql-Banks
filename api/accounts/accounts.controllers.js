7675af0602a32e60cde4245d51c4b650523e757a
MARIAM ALRASDAN <mariam.alrashdan97@gmail.com>
2023-06-20 03:47:51 +0300

    task- express M2

--- api/accounts/accounts.controllers.js
+++ api/accounts/accounts.controllers.js
@@ -1,36 +1,55 @@
-let accounts = require('../../accounts');
+let accounts = require("../../accounts");
 
-exports.accountCreate = (req, res) => {
-  const id = accounts[accounts.length - 1].id + 1;
-  const newAccount = { ...req.body, funds: 0, id };
-  accounts.push(newAccount);
-  res.status(201).json(newAccount);
-};
+const Account = require("../../db/models/Account");
 
-exports.accountDelete = (req, res) => {
-  const { accountId } = req.params;
-  const foundAccount = accounts.find((account) => account.id === +accountId);
-  if (foundAccount) {
-    accounts = accounts.filter((account) => account.id !== +accountId);
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: 'Account not found' });
+exports.accountCreate = async (req, res) => {
+  try {
+    const newAccount = await Account.create(req.body); //-> to save it in the MongoDB database
+    //async/await syntax to handle the asynchronous
+    res.status(201).json(newAccount);
+  } catch (error) {
+    res.status(500).json({ message: "somthing went wrong" });
   }
 };
 
-exports.accountUpdate = (req, res) => {
+exports.accountDelete = async (req, res) => {
   const { accountId } = req.params;
-  const foundAccount = accounts.find((account) => account.id === +accountId);
-  if (foundAccount) {
-    foundAccount.funds = req.body.funds;
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: 'Account not found' });
+  try {
+    const foundAccount = await Account.findById(accountId);
+
+    if (foundAccount) {
+      await Account.deleteOne();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Account not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "somthing went wrong" });
   }
 };
 
-exports.accountsGet = (req, res) => {
-  res.json(accounts);
+exports.accountUpdate = async (req, res) => {
+  const { accountId } = req.params;
+  try {
+    const foundAccount = await Account.findById(accountId);
+
+    if (!foundAccount) {
+      return res.status(404).json({ message: "Accpunt not found" });
+    }
+    await foundAccount.updateOne(req.body);
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(500).json({ message: "somthing went wrong" });
+  }
+};
+
+exports.accountsGet = async (req, res) => {
+  try {
+    const account = await Account.find().select("-createdAt -updatedAt");
+    return res.status(200).json(accounts);
+  } catch (error) {
+    return res.status(500).json({ message: "Somthing went wrong" });
+  }
 };
 
 exports.getAccountByUsername = (req, res) => {
@@ -38,9 +57,26 @@ exports.getAccountByUsername = (req, res) => {
   const foundAccount = accounts.find(
     (account) => account.username === username
   );
-  if (req.query.currency === 'usd') {
+  if (req.query.currency === "usd") {
     const accountInUsd = { ...foundAccount, funds: foundAccount.funds * 3.31 };
     res.status(201).json(accountInUsd);
   }
   res.status(201).json(foundAccount);
 };
+
+// exports.accountUpdate = async (req, res) => {
+//   const { accountId } = req.params;
+//   try {
+//     const foundAccount = await Account.findById(accountId);
+
+//     if (foundAccount) {
+//       await Account.findByIdAndUpdate(accountId, req.body);
+//       res.status(204).end();
+//       //204 == No Content
+//     } else {
+//       res.status(404).json({ message: "Account not found" });
+//     }
+//   } catch (error) {
+//     return res.status(500).json({ message: "somthing went wrong" });
+//   }
+// };
--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-let accounts = require('./accounts');
-const express = require('express');
+let accounts = require("./accounts");
+const express = require("express");
 const app = express();
-const accountsRoutes = require('./api/accounts/accounts.routes');
+const connectDb = require("./connectDb");
+const accountsRoutes = require("./api/accounts/accounts.routes");
+//const booksRoutes = require("./api/books/router");
+
+const dotenv = require("dotenv");
+dotenv.config(); //because i need access to the port
+
+connectDb();
 
 app.use(express.json());
-app.use('/accounts', accountsRoutes);
+app.use("/accounts", accountsRoutes);
+//app.use("", booksRoutes);
 
-app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`The application is running on localhost:${PORT}`);
 });
